d75d91bbd6ab35e635648e8be670720319447ae3
Alexandre Lins <aslinsjr@gmail.com>
2025-11-02 20:16:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ import { createSearchRoutes } from './routes/searchRoutes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 const mongo = new MongoService();
 const firebase = new FirebaseService();
