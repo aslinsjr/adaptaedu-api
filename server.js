fe06942540f67f3b8fe43eb6378e64f87811c87b
Alexandre Lins <aslinsjr@gmail.com>
2025-11-02 20:05:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://adaptaedu.vercel.app',
+  origin: 'https://adaptaedu.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
