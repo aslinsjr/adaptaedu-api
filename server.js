ac8881c740c42cf212f09dc69ebd7357257d02f6
Alexandre Lins <aslinsjr@gmail.com>
2025-11-02 20:01:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ import { createSearchRoutes } from './routes/searchRoutes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors('https://adaptaedu.vercel.app'));
+app.use(cors({
+  origin: 'https://adaptaedu.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const mongo = new MongoService();
 const firebase = new FirebaseService();
