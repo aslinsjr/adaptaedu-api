5603bb61e5c9c01b1c4d80bb10092835534a40d8
Alexandre Lins <aslinsjr@gmail.com>
2025-11-03 07:53:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:8080'
+      'http://localhost:5174'
     ];
     
     // Permite requisições sem origin (Postman, mobile apps, etc)
